5d347eaf01cc694e8e19c95474f3d91b51ef96a7
scaussin <scaussin@student.42.fr>
2020-12-04 22:41:18 +0100

    open

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+//#include <iostream>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-int main()
+//using namespace std;
+
+int main(int ac, char **av)
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (ac != 2)
+    {
+        return 1;
+    }
+
+    struct stat buf = {0};
+    int32_t fd;
+    void *file_start;
+
+    if ((fd = open(av[1], O_RDONLY)) < 0)
+        return 1;
+    if (fstat(fd, &buf) < 0)
+        return 1;
+    if (buf.st_size == 0)
+        return 1;
+    if ((file_start = mmap(nullptr, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+        return 1;
+
+
     return 0;
 }
+
