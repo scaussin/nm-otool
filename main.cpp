d880846de4ae22ffedb3abca179d3f278ad99dc5
scaussin <scaussin@student.42.fr>
2020-12-09 23:04:50 +0100

    otool + LC_SYMTAB

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <iomanip>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -8,8 +9,11 @@
 #include <vector>
 
 using namespace std;
-bool parseMachoHeader(void *dataStart);
-void parseLoadCommandSegment(void *dataStart, void *data);
+bool parseMachoHeader(char *dataStart);
+void parseLoadCommandSegment(char *dataStart, void *data);
+void hexdumpBuf(char *buf, uint64_t len);
+void parseSymtab(char *dataStart, void *data);
+
 struct s_segComHrdVecSecHrd{
     s_segComHrdVecSecHrd () : segComHrd({0}), vecSecHrd(0)
     {
@@ -42,12 +46,12 @@ int main(int ac, char **av)
     if ((file_start = mmap(nullptr, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
         return 1;
 
-    parseMachoHeader(file_start);
+    parseMachoHeader((char *)file_start);
 
     return 0;
 }
 
-bool parseMachoHeader(void *dataStart)
+bool parseMachoHeader(char *dataStart)
 {
     cout << "taille load command " << sizeof(load_command) << endl;
     mach_header_64 *h = (mach_header_64 *) dataStart;
@@ -66,31 +70,24 @@ bool parseMachoHeader(void *dataStart)
     for (int i = 0; i < nbcmds; i++)
     {
         struct load_command *lc;
-        lc = (struct load_command *) ((char*)dataStart + offset);
+        lc = (struct load_command *) (dataStart + offset);
         //cout << "command: " << lc->cmd << " size " << lc->cmdsize << endl;
         if (lc->cmd == LC_SEGMENT_64)
         {
             parseLoadCommandSegment(dataStart, lc);
         }
+        else if (lc->cmd == LC_SYMTAB)
+        {
+            parseSymtab(dataStart, lc);
+        }
 
         offset += lc->cmdsize;
-        //offset += 80;
     }
-    void *end = (char *)(dataStart) + offset;
+    void *end = (char *)(dataStart) + offset ;
     return true;
 }
 
-/*
- struct s_segComHrdVecSecHrd{
-    segment_command_64 segComHrd;
-    vector<section_64> vecSecHrd;
-};
-
-vector<s_segComHrdVecSecHrd> \;
-
- */
-
-void parseLoadCommandSegment(void *dataStart, void *data)
+void parseLoadCommandSegment(char *dataStart, void *data)
 {
     s_segComHrdVecSecHrd a;
     memcpy(&(a.segComHrd), data, sizeof(segment_command_64));
@@ -102,24 +99,44 @@ void parseLoadCommandSegment(void *dataStart, void *data)
         section_64 *section64 = (section_64 *) ((char *)data + sizeof(segment_command_64) + i * sizeof(section_64));
         a.vecSecHrd.push_back(*section64);
 
-        cout <<"    section64: " << section64->segname << "." << section64->sectname << " addr: " << section64->addr << " off: " << section64->offset << endl;
+        if (string(section64->segname) + string(section64->sectname) == "__TEXT__text")
+        {
+            hexdumpBuf(dataStart + section64->offset, section64->size);
+        }
+
+        //cout <<"    section64: " << section64->segname << "." << section64->sectname << " addr: " << section64->addr << " off: " << section64->offset << endl;
     }
 
     vecStructSegComHrdVecSecHrd.push_back(a);
+}
 
-    /*if (loadCommandSegment.nsects)
+
+void parseSymtab(char *dataStart, void *data)
+{
+    symtab_command sc;
+    memcpy(&sc, data, sizeof(symtab_command));
+
+
+    cout << "offset: " << sc.symoff << " | nsyms: " << sc.nsyms << " | stroff: " << sc.stroff<< " | strsize: " << sc.strsize << endl;
+
+
+}
+
+void hexdumpBuf(char *buf, uint64_t len)
+{
+    cout << endl;
+    for (uint64_t i = 0; i < len; i++)
     {
+        cout << std::setw(2) << std::setfill('0') << std::hex << (uint16_t) ((uint8_t) buf[i]) << " " << std::flush;
 
-        section_64 *section64 = (section_64 *) ((char *)data + sizeof(segment_command_64));
-        cout << "offset: " << loadCommandSegment.fileoff << " | addr: " << loadCommandSegment.vmaddr << " | size: " << loadCommandSegment.filesize << " | section64: " << section64->sectname << endl;
-
+        if (i % 8 == 7 && i % 16 != 15)
+        {
+            cout << " " << std::flush;
+        }
+        else if (i % 16 == 15)
+        {
+            cout << endl << std::flush;
+        }
     }
-    else
-    {
-        cout << "offset: " << loadCommandSegment.fileoff << " | addr: " << loadCommandSegment.vmaddr << " | size: " << loadCommandSegment.filesize << endl;
-    }*/
-
-   // cout << "offset: " << loadCommandSegment.fileoff << " | addr: " << loadCommandSegment.vmaddr << " | size: " << loadCommandSegment.filesize << " | section64: " << section64 << endl;
-    //cout << addr << endl;
-    //section_64 *section = data + loadCommandSegment->fileoff;
+    cout << endl << std::dec << std::flush;
 }
